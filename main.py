af20077a6902aeb363ce0baad8b1fddf5d402062
polioan <rognash.yekan@yandex.ru>
2022-08-16 03:20:05 +0300

    reducing the amount of code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#the bot was made in diskord.py 1.7.3		бот был сделан на diskord.py 1.7.3
+# the bot was made in diskord.py 1.7.3
 import CONFIG
 import discord
 from discord.ext import tasks
@@ -7,21 +7,18 @@ from datetime import datetime
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
-@tasks.loop(seconds = CONFIG.UPDATE_TIME)
+@tasks.loop(seconds=CONFIG.UPDATE_TIME)
 async def mainLoop():
-	try:
-		for guild in client.guilds:
-			for member in guild.members:
-				if member is not None:
-					if member.activity is not None:
-						if member.activity.name is not None:
-							for i in range(len(CONFIG.BANNED_GAMES)):
-								if member.activity.name.lower() == CONFIG.BANNED_GAMES[i].lower():
-									if (datetime.utcnow() - member.activity.start).total_seconds() > CONFIG.BAN_TIME:
-										await member.send(CONFIG.BAN_MESSAGES[i])
-										await member.ban(delete_message_days=0, reason=CONFIG.BAN_REASONS[i])
-	except exception as e:
-		print("Error", e)
+    try:
+        for guild in client.guilds:
+            for member in guild.members:
+                if member and member.activity and member.activity.name:
+                    for i in range(len(CONFIG.BANNED_GAMES)):
+                        if (member.activity.name.lower() == CONFIG.BANNED_GAMES[i].lower()) and ((datetime.utcnow() - member.activity.start).total_seconds() > CONFIG.BAN_TIME):
+                            await member.send(CONFIG.BAN_MESSAGES[i])
+                            await member.ban(delete_message_days=0, reason=CONFIG.BAN_REASONS[i])
+    except exception as e:
+        print("Error", e)
 
 mainLoop.start()
-client.run(CONFIG.TOKEN)							
+client.run(CONFIG.TOKEN)
