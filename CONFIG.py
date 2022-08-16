4bbdf6c8ba00959e43bb915a0470ea0a30d5774b
polioan <rognash.yekan@yandex.ru>
2022-08-16 03:38:40 +0300

    text has been shortened

--- CONFIG.py
+++ CONFIG.py
@@ -1,6 +1,6 @@
-TOKEN = "your token		ваш токен"
-UPDATE_TIME = 0.5 #how many times per second does the script turn on (for large servers, set the value more)		сколько раз в секунду включается скрипт	(для больших серверов ставьте значение больше)
-BAN_TIME = 1800 #ban time in seconds		время наступления бана в секундах
-BANNED_GAMES = ["league of legends"] #list of games to search for		список игр для поиска
-BAN_MESSAGES = ["30 минут прошло, а ты всё ещё играешь в лигу. Бан."] #список сообщения при бане для каждой игре		list of messages when ban for each game	
-BAN_REASONS = ["Играл в лигу"] #reasons for bans for each game		причины банов к каждой игре
+TOKEN = "your token"
+UPDATE_TIME = 0.5 #how many times per second does the script turn on (for large servers, set the value more)
+BAN_TIME = 1800 #ban time in seconds
+BANNED_GAMES = ["league of legends"] #list of games to search for
+BAN_MESSAGES = ["30 минут прошло, а ты всё ещё играешь в лигу. Бан."] #list of messages when ban for each game	
+BAN_REASONS = ["Играл в лигу"] #reasons for bans for each game
